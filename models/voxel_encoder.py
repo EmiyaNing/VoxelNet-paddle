1c0b61e577ceae15028c33cc3eeba636a57e47d6
EmiyaNing <ningkangl@icloud.com>
2021-09-08 14:42:50 +0800

    Sub some unessary part of those model

--- models/voxel_encoder.py
+++ models/voxel_encoder.py
@@ -43,7 +43,7 @@ class VFELayer(nn.Layer):
         super().__init__()
         self.name  = name
         self.units = out_channels // 2
-        self.linear= nn.Linear(in_channels, self.units, bias_attr=False)
+        self.linear= nn.Linear(in_channels, self.units)
         self.norm  = nn.BatchNorm1D(self.units, momentum=0.01, epsilon=1e-3)
         self.relu  = nn.ReLU()
 
@@ -86,9 +86,9 @@ class VoxelFeatureExtractor(nn.Layer):
         self.VFE_layerlist = nn.LayerList()
         for i in range(self.vfe_number-1):
             self.VFE_layerlist.append(VFELayer(num_filters[i], num_filters[i+1]))
-        self.linear     = nn.Linear(num_filters[-1], num_filters[-1], bias_attr=False)
-        self.norm       = nn.BatchNorm1D(num_filters[-1], momentum=0.01, epsilon=1e-3)
-        self.relu       = nn.ReLU()
+        #self.linear     = nn.Linear(num_filters[-1], num_filters[-1], bias_attr=False)
+        #self.norm       = nn.BatchNorm1D(num_filters[-1], momentum=0.01, epsilon=1e-3)
+        #self.relu       = nn.ReLU()
 
     def voxel_index(self, features, coor):
         dim           = features.shape[-1]
@@ -126,9 +126,9 @@ class VoxelFeatureExtractor(nn.Layer):
         for layer in self.VFE_layerlist:
             features = layer(features)
             features = features * mask
-        x = self.linear(features)
-        x = paddle.transpose(self.norm(paddle.transpose(x, perm=[0, 2, 1])), perm=[0, 2, 1])
-        x = self.relu(x)
+        #x = self.linear(features)
+        #x = paddle.transpose(self.norm(paddle.transpose(x, perm=[0, 2, 1])), perm=[0, 2, 1])
+        #x = self.relu(x)
         x = x * mask
         voxelwise = paddle.max(x, axis=1)
         voxelwise = self.voxel_index(voxelwise, coor)
--- unit_test/model_test.py
+++ unit_test/model_test.py
@@ -6,6 +6,19 @@ from config import get_config
 
 cfg = get_config()
 
+def print_model_named_params(model):
+    print('----------------------------------')
+    for name, param in model.named_parameters():
+        print(name, param.shape)
+    print('----------------------------------')
+
+
+def print_model_named_buffers(model):
+    print('----------------------------------')
+    for name, param in model.named_buffers():
+        print(name, param.shape)
+    print('----------------------------------')
+
 def test_model():
     data   = paddle.rand([32, 35, 4])
     num    = paddle.randint(0, 35, [32])
@@ -15,5 +28,10 @@ def test_model():
     print('p_map.shape = ', p_map.shape)
     print('r_map.shape = ', r_map.shape)
 
+def show_model():
+    model = VoxelNet(cfg)
+    print_model_named_params(model)
+
 if __name__ == '__main__':
-    test_model()
+    #test_model()
+    show_model()
